76c3a8c0e64ede9926ccd55cd31a15d33d42f55f
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-08 00:50:15 +0530

    2 pages created for the sidebar and navbar components and plugins made for backgound color which is used inthe layout page of the app folder.

--- connectify/src/app/(root)/(home)/layout.tsx
+++ connectify/src/app/(root)/(home)/layout.tsx
@@ -6,7 +6,11 @@ const HomeLayout = ({children} : {children: ReactNode}) => {
       Navbar
       <div className='flex'>
         Sidebar
-        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'></section>
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'>
+            <div className='w-full'>
+                {children}
+            </div>
+        </section>
       </div>
     </main>
   )
