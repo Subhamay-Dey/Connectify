f09e691eecb403af61f4a59f1a54363b06e89de8
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-08 02:19:55 +0530

    a nasty bug was debugged.

--- connectify/src/components/Sidebar.tsx
+++ connectify/src/components/Sidebar.tsx
@@ -1,8 +1,26 @@
+"use client";
+
 import React from 'react'
+import { sidebarLinks } from '../../constants'
+import { usePathname } from 'next/navigation'
 
 const Sidebar = () => {
+
+  const pathname = usePathname();
+
   return (
-    <div>Sidebar</div>
+    <section className='sticky left-0 top-0 flex h-screen w-fit flex-col justify-between bg-dark-1 p-6 pt-28 text-white max-sm:hidden lg:w-[264px]'>
+      <div className='flex flex-col gap-6'>
+        {sidebarLinks.map((index) => {
+          const isActive = pathname === index!.route || pathname.startsWith(index!.route);
+          return(
+            <>
+
+            </>
+          )
+        })}
+      </div>
+    </section> 
   )
 }
 
