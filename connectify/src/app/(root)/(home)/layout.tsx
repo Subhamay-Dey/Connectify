0c79a8344ee5771ff501f1676b065597382a8eb3
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-07 23:59:35 +0530

    ui of home page is updated and also responsive

--- connectify/src/app/(root)/(home)/layout.tsx
+++ connectify/src/app/(root)/(home)/layout.tsx
@@ -2,8 +2,12 @@ import React, { ReactNode } from 'react'
 
 const HomeLayout = ({children} : {children: ReactNode}) => {
   return (
-    <main>
-      
+    <main className='relative'>
+      Navbar
+      <div className='flex'>
+        Sidebar
+        <section className='flex min-h-screen flex-1 flex-col px-6 pb-6 pt-28 max-md:pb-14 sm:px-14'></section>
+      </div>
     </main>
   )
 }
