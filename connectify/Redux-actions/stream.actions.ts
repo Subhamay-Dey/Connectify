854ee78d660a53f6d8dd818a57c5d965adbff5e5
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-10 00:00:04 +0530

    updated.

--- connectify/Redux-actions/stream.actions.ts
+++ connectify/Redux-actions/stream.actions.ts
@@ -17,5 +17,5 @@ const tokenProvider = async () => {
 
     const exp = Math.round(new Date().getTime()/1000) + 60 * 60;
 
-    const issued = Math.floor(Date.now() /1000) - 60 ;
+    const issued = Math.floor(Date.now() /1000) - 60
 }
