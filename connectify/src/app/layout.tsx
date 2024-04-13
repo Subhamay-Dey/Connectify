1939b22528a54fcd896b21d5793f9b649efd4bcc
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-13 22:04:59 +0530

    app layout updated with the clerk provider properties.

--- connectify/src/app/layout.tsx
+++ connectify/src/app/layout.tsx
@@ -18,7 +18,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
+    <ClerkProvider
+      appearance={{
+        layout: {
+          logoImageUrl: '/icons/yoom-logo.svg',
+          socialButtonsVariant: 'iconButton'
+        },
+        variables: {
+          colorText: "#fff",
+          colorPrimary: "#0E78F9",
+          colorBackground: "#1C1F2E",
+          colorInputBackground: "#252A41",
+          colorInputText: "#fff",
+        },
+      }}
+    >
       <html lang="en">
         <body className={`${inter.className} bg-dark-2`}>
           {children}
