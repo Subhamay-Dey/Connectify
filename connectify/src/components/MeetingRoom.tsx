9eeb2357f5677a26cf0f278bed8838ff2b643b76
Subhamay-Dey <ncdey1966@gmail.com>
2024-04-14 11:53:12 +0530

    meeting room and meetig setup updated as well the toaster.

---
+++ connectify/src/components/MeetingRoom.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const MeetingRoom = () => {
+
+  return (
+    <div>MeetingRoom</div>
+  )
+}
+
+export default MeetingRoom
--- connectify/src/components/MeetingSetup.tsx
+++ connectify/src/components/MeetingSetup.tsx
@@ -1,8 +1,15 @@
-import React from 'react'
+import { VideoPreview } from '@stream-io/video-react-sdk'
+import React, { useState } from 'react'
 
 const MeetingSetup = () => {
+
+  const [ isMiccamToggledOn, setIsMicCamToggledOn] = useState(false);
+
   return (
-    <div>MeetingSetup</div>
+    <div className='flex h-screen w-full flex-col items-center justify-center gap-3 text-white'>
+      <h1 className='text-2xl font-bold'>Setup</h1>
+      <VideoPreview/>
+    </div>
   )
 }
 
